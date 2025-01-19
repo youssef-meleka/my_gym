22581a1023d79f6bb6a7f5050d21c77161b8875a
youssef-meleka <youssefatef00@gmail.com>
2025-01-19 13:50:53 +0200

    adding softDelets to models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
